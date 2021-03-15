f35d69304e8606a932c8f715a96acee3f81731a5
Dandi Andrian <61398138+Danphyxia@users.noreply.github.com>
2021-03-15 09:13:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Honkai War</title>
+    <style>
+        #hero_wrapper {
+            background-image: url("img/p.png")
+
+        }
+    #footer {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+
+    /* section */
+    section {
+            padding-top: 50px;
+            padding-bottom: 50px;
+    }
+    #home {
+            padding-top: 0px;
+            padding-bottom: 0px;
+    }
+    #about, #gallery, #contact {
+            padding-top: 350px;
+            padding-bottom: 350px;
+    }
+    /*button*/
+    .btn-primary {
+        background-color: cyan;
+    }
+    /*Paralax*/
+    .parallax{
+        background-image: url('img/5.png');
+
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        font-size: 30px;
+    }
+    .parallax2{
+        background-image: url('img/6.png');
+
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        font-size: 30px;
+    }
+    .parallax3{
+        background-image: url('img/7.png');
+
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        font-size: 30px;
+    }
+    #footer-copyright{
+        font-size: 25px;
+        font-color: white;
+    }
+    </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+
+<section id="home" >
+<div id="mycaro" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/4.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  
+  <button class="carousel-control-prev" type="button" data-bs-target="#mycaro"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#mycaro"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+    </section>
+    <section id="hero_section" class="bg-secondary">
+  <div class="hero_wrapper">
+    <div class="container">
+      <div class="hero_section">
+        <div class="row">
+          <div class="col-lg-5 col-sm-7">
+            <div class="top_left_cont zoomIn wow animated" font-size: 20px; background-color: black;> 
+            <div class="container">
+            <div class="row">
+                <h2> Ayo <strong> Selamatkan </strong> Mereka </strong> </h2>
+              <p>Takdir mereka ada di tanganmu Kapten, Game Visual Novel yang diambil dari story game Honkai Impact 3 kini telah hadir, untuk memulai permainan pilihlah salah satu dari 3 Gadis ini untuk menjadi rekan perjalananmu dalam menyelamatkan dunia serta menyelamatkan takdir tragis yang tengah menunggu mereka setiap pilihan yang kamu ambil sangat berpengaruh untuk hidup mereka jadi pilihlah dengan bijak Setelah memilih resource akan mendownload otomatis story dari karakter yang kapten pilih</p> <a href="#"> </div>
+          </a>
+            </div>
+           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <section id="about" class="bg-secondary parallax">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <h1>Zhuge Kongming</h1>
+                    <p>Seorang gadis suci dari jaman kerajaan, dirinya dipercaya untuk memimpin sebuah negara besar di usia muda dan telah berjasa menyelamatkan kerajaan dari ancaman binatang iblis yang kini telah ia segel, namun segelnya terlepas dan membuatnya harus kehilangan teman kesayangannya hingga membuat kepribadiannya berubah </p>
+                <br/><br/>
+                <button type="button" class="btn btn-primary btn-lg">Pilih</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="gallery" class="bg-success parallax2">
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <h1>Herrscher of Thunder</h1>
+                    <p>Seorang gadis remaja yang menjadi korban tragedi Penyergan honkai hingga membuat seluruh keluarganya meninggal, dan hanya dia yang tersisa namun dia diselamatkan oleh gadis yang ceria yang saaat ini menjadi teman dekatnya saat ia hendak terjatuh ke jurang, Gadis yang dulu dikenal sebagai Raiden Mei kini telah mengorbankan diirnya untuk menanggung tanggung jawab sebagai Herrscher of Thunder untuk menyelamatkan satu-satunya teman berharga yang dia miliki</p><br/><br/>
+                <button type="button" class="btn btn-primary btn-lg">Pilih</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    </section>
+
+    <section id="contact" class="bg-warning parallax3">
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <h1>Herrscher of Sentience</h1>
+                    <p>
+                        Jelemaan dari mahluk legenda burung Phoenix seorang gadis yang telah hidup ribuan tahun dan juga merupakan manusia tertua serta terkuat ke-2 di dunia, karena usia nya yang sudah panjang ia telah mengalami banyak pertemuan dan perpisahan yang tak terhitung jumlah nya,semua emosi negatif yang terus ia tahan selama ribuan tahun kini kian menghitam dan menggumpal hingga akhirnya ia kehilangan akal sehingga membuat tubuhnya yang diambil alih oleh Herrscher of Sentience</p>
+                        <br/><br/>
+                <button type="button" class="btn btn-primary btn-lg">Pilih</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <footer class="page-footer page-footer-expand-lg  page-footer-dark bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col s6"><a><img src="img/mihoyo.png" alt=""></a> 
+                <a><img src="img/Hi3.png" alt=""></a>
+            </div>
+                    <div class="col s6">
+                        <ul>
+                            <li> <a href="#"> Privacy Policy </a> </li>
+                            <li> <a href="#"> Terms of Use </a> </li>
+                            <li> <a href="#"> About Us </a> </li>
+                            <li> <a href="#"> Contact </a> </li>
+                        </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright footer-copyright-expand-lg footer-copyright-dark bg-dark" >
+        <div class= "container ">
+       <p> copyright © 2016 miHoYo All Rights Reserved </p>
+        </div>
+
+    </div>
+  </body>
+</html>
